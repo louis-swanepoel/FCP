d50a166962b053f083b53c9ce6b4c8f473cc14b1
lswanepoel <98822407+louis-swanepoel@users.noreply.github.com>
2022-04-18 18:43:13 +0100

    created functions so user can choose when to see halls and nationality data for SIR aswell as choosing which SIR vsls to see in the time series

--- Metrics.py
+++ Metrics.py
@@ -78,119 +78,120 @@ def CollectSIRdata():
       
    
 # returns--  SIRlistNorth, SIRlistEast, SIRlistSouth , SIRlistWest where each is a nested list of SIR data 
-def DailyDataSIR():
+def DailyDataSIR(Day,CollectDataDay):
     
-    # Create Lists to collect S, I and R statuses in bins 
-    SlistPop = []
-    IlistPop = []
-    RlistPop = []
+    if Day == CollectDataDay:
     
+        # Create Lists to collect S, I and R statuses in bins 
+        SlistPop = []
+        IlistPop = []
+        RlistPop = []
         
-    # List of SIR for each halls 
-    SIRlistNorth = [[],[],[]]
-    SIRlistEast = [[],[],[]]
-    SIRlistWest = [[],[],[]]
-    SIRlistSouth = [[],[],[]]
-
-    # Iterate through each student
-    for StudentNumber in range(s.PopulationSize):
-
-        # Add each students SIR status to the correct bin so now the bins have collected all students
-        if s.StudentObjects[StudentNumber].SIR == 'S':
-            SlistPop.append(s.StudentObjects[StudentNumber]) 
-
-            if s.StudentObjects[StudentNumber].Halls == 'North':
-                SIRlistNorth[0].append(s.StudentObjects[StudentNumber])           
-            elif s.StudentObjects[StudentNumber].Halls == 'East':
-                SIRlistEast[0].append(s.StudentObjects[StudentNumber])
-            elif s.StudentObjects[StudentNumber].Halls == 'West':
-                SIRlistWest[0].append(s.StudentObjects[StudentNumber])
-            elif s.StudentObjects[StudentNumber].Halls == 'South':
-                SIRlistSouth[0].append(s.StudentObjects[StudentNumber])
-            else:
-                ValueError('Student must have an SIR value')
-
-
-        elif s.StudentObjects[StudentNumber].SIR == 'I':
-            IlistPop.append(s.StudentObjects[StudentNumber])
-
-            if s.StudentObjects[StudentNumber].Halls == 'North':
-                SIRlistNorth[1].append(s.StudentObjects[StudentNumber])           
-            elif s.StudentObjects[StudentNumber].Halls == 'East':
-                SIRlistEast[1].append(s.StudentObjects[StudentNumber])
-            elif s.StudentObjects[StudentNumber].Halls == 'West':
-                SIRlistWest[1].append(s.StudentObjects[StudentNumber])
-            elif s.StudentObjects[StudentNumber].Halls == 'South':
-                SIRlistSouth[1].append(s.StudentObjects[StudentNumber])
-            else:
-                ValueError('Student must have an SIR value')
-
-        elif s.StudentObjects[StudentNumber].SIR == 'R':
-            RlistPop.append(s.StudentObjects[StudentNumber])
-
-            if s.StudentObjects[StudentNumber].Halls == 'North':
-                SIRlistNorth[2].append(s.StudentObjects[StudentNumber])           
-            elif s.StudentObjects[StudentNumber].Halls == 'East':
-                SIRlistEast[2].append(s.StudentObjects[StudentNumber])
-            elif s.StudentObjects[StudentNumber].Halls == 'West':
-                SIRlistWest[2].append(s.StudentObjects[StudentNumber])
-            elif s.StudentObjects[StudentNumber].Halls == 'South':
-                SIRlistSouth[2].append(s.StudentObjects[StudentNumber])
-            else:
-                ValueError('Student must have an SIR value')
-
-        else:
-            raise ValueError('Student must have an SIR value')
             
-    return  (SIRlistNorth), (SIRlistEast), (SIRlistSouth) , (SIRlistWest)
+        # List of SIR for each halls 
+        SIRlistNorth = [[],[],[]]
+        SIRlistEast = [[],[],[]]
+        SIRlistWest = [[],[],[]]
+        SIRlistSouth = [[],[],[]]
+    
+        # Iterate through each student
+        for StudentNumber in range(s.PopulationSize):
+    
+            # Add each students SIR status to the correct bin so now the bins have collected all students
+            if s.StudentObjects[StudentNumber].SIR == 'S':
+                SlistPop.append(s.StudentObjects[StudentNumber]) 
+    
+                if s.StudentObjects[StudentNumber].Halls == 'North':
+                    SIRlistNorth[0].append(s.StudentObjects[StudentNumber])           
+                elif s.StudentObjects[StudentNumber].Halls == 'East':
+                    SIRlistEast[0].append(s.StudentObjects[StudentNumber])
+                elif s.StudentObjects[StudentNumber].Halls == 'West':
+                    SIRlistWest[0].append(s.StudentObjects[StudentNumber])
+                elif s.StudentObjects[StudentNumber].Halls == 'South':
+                    SIRlistSouth[0].append(s.StudentObjects[StudentNumber])
+                else:
+                    ValueError('Student must have an SIR value')
+    
+    
+            elif s.StudentObjects[StudentNumber].SIR == 'I':
+                IlistPop.append(s.StudentObjects[StudentNumber])
+    
+                if s.StudentObjects[StudentNumber].Halls == 'North':
+                    SIRlistNorth[1].append(s.StudentObjects[StudentNumber])           
+                elif s.StudentObjects[StudentNumber].Halls == 'East':
+                    SIRlistEast[1].append(s.StudentObjects[StudentNumber])
+                elif s.StudentObjects[StudentNumber].Halls == 'West':
+                    SIRlistWest[1].append(s.StudentObjects[StudentNumber])
+                elif s.StudentObjects[StudentNumber].Halls == 'South':
+                    SIRlistSouth[1].append(s.StudentObjects[StudentNumber])
+                else:
+                    ValueError('Student must have an SIR value')
+    
+            elif s.StudentObjects[StudentNumber].SIR == 'R':
+                RlistPop.append(s.StudentObjects[StudentNumber])
+    
+                if s.StudentObjects[StudentNumber].Halls == 'North':
+                    SIRlistNorth[2].append(s.StudentObjects[StudentNumber])           
+                elif s.StudentObjects[StudentNumber].Halls == 'East':
+                    SIRlistEast[2].append(s.StudentObjects[StudentNumber])
+                elif s.StudentObjects[StudentNumber].Halls == 'West':
+                    SIRlistWest[2].append(s.StudentObjects[StudentNumber])
+                elif s.StudentObjects[StudentNumber].Halls == 'South':
+                    SIRlistSouth[2].append(s.StudentObjects[StudentNumber])
+                else:
+                    ValueError('Student must have an SIR value')
+    
+            else:
+                raise ValueError('Student must have an SIR value')
+                
+        return  (SIRlistNorth), (SIRlistEast), (SIRlistSouth) , (SIRlistWest)
    
-def SIRinfoFilter(SIRstatus, Origin, Name, Halls, Course):
+def SIRinfoFilter(SIRstatus, Origin, Name, Halls, Course, Day,CollectDataDay):
     
-    # Reuses CreateDataframe function to produce a data frame of all students data
-    StudentInfoDataFrame = CreateDataFrame()
-    
-    #   Checks to see if each respective attribute to be filtered by was defined when inputting the function 
-    if isinstance(SIRstatus,str) == True:    
+    if Day == CollectDataDay:
         
-        # If it was defined then it adds the filtering condition tot the dataframe
-        StudentInfoDataFrameFiltered = StudentInfoDataFrame[(StudentInfoDataFrame['SIR'] == SIRstatus)]   
-    else:
+        # Reuses CreateDataframe function to produce a data frame of all students data
+        StudentInfoDataFrame = CreateDataFrame()
         
-        # If no infomation was given to filter then it does not filter it by the variable and says so to the user       
-        StudentInfoDataFrameFiltered = StudentInfoDataFrame[~(StudentInfoDataFrame['SIR'] == SIRstatus)] 
-        print('No SIR val Specified')
-    
-    # These steps are repeated for each attribute that could be filtered by  
-    if isinstance(Origin,str) == True:       
-        StudentInfoDataFrameFiltered = StudentInfoDataFrameFiltered[(StudentInfoDataFrameFiltered['Origin'] == Origin) ]
-    else:        
-        StudentInfoDataFrameFiltered = StudentInfoDataFrameFiltered[~(StudentInfoDataFrameFiltered['Origin'] == Origin) ]
-        print('No Origin val Specified') 
-    
-    if isinstance(Name,str) == True:       
-        StudentInfoDataFrameFiltered = StudentInfoDataFrameFiltered[(StudentInfoDataFrameFiltered['Name'] == Name) ]
-    else:         
-        StudentInfoDataFrameFiltered = StudentInfoDataFrameFiltered[~(StudentInfoDataFrameFiltered['Name'] == Name) ]
-        print('No name val Specified')
-    
-    if isinstance(Halls,str) == True:
-        StudentInfoDataFrameFiltered = StudentInfoDataFrameFiltered[(StudentInfoDataFrameFiltered['Halls'] == Halls) ]
-    else:         
-        StudentInfoDataFrameFiltered = StudentInfoDataFrameFiltered[~(StudentInfoDataFrameFiltered['Halls'] == Halls) ]
-        print('No Halls val Specified')     
+        #   Checks to see if each respective attribute to be filtered by was defined when inputting the function 
+        if isinstance(SIRstatus,str) == True:    
+            
+            # If it was defined then it adds the filtering condition tot the dataframe
+            StudentInfoDataFrameFiltered = StudentInfoDataFrame[(StudentInfoDataFrame['SIR'] == SIRstatus)]   
+        else:
+            
+            # If no infomation was given to filter then it does not filter it by the variable and says so to the user       
+            StudentInfoDataFrameFiltered = StudentInfoDataFrame[~(StudentInfoDataFrame['SIR'] == SIRstatus)] 
+            
         
-    if isinstance(Course,str) == True:
-        StudentInfoDataFrameFiltered = StudentInfoDataFrameFiltered[(StudentInfoDataFrameFiltered['Course'] == Course) ]
-    else:        
-        StudentInfoDataFrameFiltered = StudentInfoDataFrameFiltered[~(StudentInfoDataFrameFiltered['Course'] == Course) ]
-        print('No Course val Specified') 
-    
-    # The filtered dataframe is returned
-    return  StudentInfoDataFrameFiltered    
+        # These steps are repeated for each attribute that could be filtered by  
+        if isinstance(Origin,str) == True:       
+            StudentInfoDataFrameFiltered = StudentInfoDataFrameFiltered[(StudentInfoDataFrameFiltered['Origin'] == Origin) ]
+        else:        
+            StudentInfoDataFrameFiltered = StudentInfoDataFrameFiltered[~(StudentInfoDataFrameFiltered['Origin'] == Origin) ]
+            
+        
+        if isinstance(Name,str) == True:       
+            StudentInfoDataFrameFiltered = StudentInfoDataFrameFiltered[(StudentInfoDataFrameFiltered['Name'] == Name) ]
+        else:         
+            StudentInfoDataFrameFiltered = StudentInfoDataFrameFiltered[~(StudentInfoDataFrameFiltered['Name'] == Name) ]
+            
+        
+        if isinstance(Halls,str) == True:
+            StudentInfoDataFrameFiltered = StudentInfoDataFrameFiltered[(StudentInfoDataFrameFiltered['Halls'] == Halls) ]
+        else:         
+            StudentInfoDataFrameFiltered = StudentInfoDataFrameFiltered[~(StudentInfoDataFrameFiltered['Halls'] == Halls) ]
+                 
+            
+        if isinstance(Course,str) == True:
+            StudentInfoDataFrameFiltered = StudentInfoDataFrameFiltered[(StudentInfoDataFrameFiltered['Course'] == Course) ]
+        else:        
+            StudentInfoDataFrameFiltered = StudentInfoDataFrameFiltered[~(StudentInfoDataFrameFiltered['Course'] == Course) ]
+             
+        
+        # The filtered dataframe is returned
+        return  StudentInfoDataFrameFiltered    
                          
-SIRlistNorth, SIRlistEast, SIRlistSouth , SIRlistWest  = DailyDataSIR()
-
-StudentInfoDataFrame = CreateDataFrame()
 
 #### Graphing
 
@@ -221,7 +222,112 @@ def plotlinegraph(SIRSeries):
     SIRSeries.plot('Date', 'I')
     SIRSeries.plot('Date', 'R')
     SIRSeries.show()
-       
+
+# Bar chart for halls SIR data
+def BarChartHalls(Day, DateRange,CollectDataDay):
+    if Day == CollectDataDay:
+        
+        (SIRlistNorth), (SIRlistEast), (SIRlistSouth) , (SIRlistWest) = DailyDataSIR(Day,Day)
+        
+        SIRinputChoice = input('input 0,1,2 for SIR y axis in Halls plot:')
+        if SIRinputChoice == '0':
+            yAxisName = "Susceptible"
+        elif SIRinputChoice == '1':
+            yAxisName = "Infected"
+        else:
+           yAxisName = "Recovered"
+           
+        SIR = [len(SIRlistNorth[int(SIRinputChoice)]), len(SIRlistEast[int(SIRinputChoice)]), len(SIRlistSouth[int(SIRinputChoice)]) , len(SIRlistWest[int(SIRinputChoice)])] 
+        Halls = "North","South","East","West"
+    
+        plt.bar(Halls,SIR)
+        plt.title('Halls infection data:'+ str(DateRange[Day]))
+        plt.xlabel('Halls')
+        plt.ylabel(yAxisName)
+        plt.show()   
+        
+def BarChartCountries(Day, DateRange,CollectDataDay):
+    
+    if Day == CollectDataDay:
+        
+        countries = 'China' , 'US' , 'UK' , 'EU'
+
+        SIRinputChoice = input('input S,I,R for SIR vals in y axis for countries plot:')
+        if SIRinputChoice == 'S':
+            yAxisName = "Susceptible"
+        elif SIRinputChoice == 'I':
+            yAxisName = "Infected"
+        else:
+           yAxisName = "Recovered"
+        
+        china = len(SIRinfoFilter(SIRinputChoice, 'China', None, None, None, Day,Day))
+        us = len(SIRinfoFilter(SIRinputChoice, 'US', None, None, None, Day,Day))
+        uk = len(SIRinfoFilter(SIRinputChoice, 'UK', None, None, None, Day,Day))
+        eu = len(SIRinfoFilter(SIRinputChoice, 'EU', None, None, None, Day,Day))
+        
+        SIR = [china,us,uk,eu]
+    
+        plt.bar(countries,SIR)
+        plt.title('Nationality infection data:'+ str(DateRange[Day]))
+        plt.xlabel('Nationality')
+        plt.ylabel(yAxisName)
+        plt.show()   
+def MainTimeSeriesPlot(SIRSeries):
+    
+    # Plotting the time series for certain user commands
+    SIRtimeseries = input('what would you like to see 1 var 2 var or 3 var against one another: ')
+
+    if SIRtimeseries == '1':
+        q = input('which one would you like to see (S,I,R): ')
+        if q == 'S':
+            SIRSeries['S'].plot()
+            plt.show()
+        elif q == 'I':
+            SIRSeries['I'].plot()
+            plt.show()
+        elif q == 'R':
+            SIRSeries['R'].plot()
+            plt.show()
+    elif SIRtimeseries == '2':
+        q = input('which one would you like to see (S,I,R): ')
+        if q == 'S':
+            SIRSeries['S'].plot()
+            q = input('which other one would you like to see (S,I,R): ')
+            if q == 'S':
+                SIRSeries['S'].plot()
+                plt.show()
+            elif q == 'I':
+                SIRSeries['I'].plot()
+                plt.show()
+            elif q == 'R':
+                SIRSeries['R'].plot()
+                plt.show()
+        elif q == 'I':
+            SIRSeries['I'].plot()
+            q = input('which other one would you like to see (S,I,R): ')
+            if q == 'S':
+                SIRSeries['S'].plot()
+                plt.show()
+            elif q == 'I':
+                SIRSeries['I'].plot()
+                plt.show()
+            elif q == 'R':
+                SIRSeries['R'].plot()
+                plt.show()
+        elif q == 'R':
+            SIRSeries['R'].plot()
+            q = input('which other one would you like to see (S,I,R): ')
+            if q == 'S':
+                SIRSeries['S'].plot()
+                plt.show()
+            elif q == 'I':
+                SIRSeries['I'].plot()
+                plt.show()
+            elif q == 'R':
+                SIRSeries['R'].plot()
+                plt.show()
+    else:
+        SIRSeries.plot()        
     ###### TESTING
 
 
