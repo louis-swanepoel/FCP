6c83bba00e3d41aab036eeaf74462a40d1ab6813
lswanepoel <98822407+louis-swanepoel@users.noreply.github.com>
2022-04-18 18:42:32 +0100

    created option for user defined day to collect data

--- Main.py
+++ Main.py
@@ -17,10 +17,29 @@ import random as r
 
 # Todays Date 
 Today = d.today()
+Today = Today.strftime("%d/%m/%Y")
+CustomDate = '17/09/2022' 
 
 # Produces an array of the date from september until now to intiate the SIR time series 
-DateRange= pd.date_range('17/09/2021', Today.strftime("%d/%m/%Y"))
+DateRange= pd.date_range('17/09/2021', CustomDate)
 
+# Day when you want data collected- TESTING TO SEE IF DAY OF DATA COLLECTION CAN BE SELECTED BY THE USER
+# CollectDataDay = input('as %d-%m-%Y 00:00:00, input the date you want data collected:')
+# counter = 0
+# for date in DateRange:
+#     counter += 1
+#     if date == CollectDataDay:
+#         CollectDataDay = counter
+#         continue
+
+dataCollection = input('would you like to collect any other data? yes/no ')
+
+
+if dataCollection == 'yes':
+    CollectDataDay = int(input('What day would you like the data collected '))
+else:   
+    CollectDataDay = -1
+        
 # Creater the SIR time series from the start of universirty 2021 until now
 SIRSeries = pd.DataFrame(DateRange, columns=['Date'])
 
@@ -32,6 +51,10 @@ R = []
 # Going through each days until now 
 for Day in range(0,len(DateRange)):
     
+    # Collecting Data example 
+    m.BarChartHalls(Day, DateRange, CollectDataDay)
+    m.BarChartCountries(Day, DateRange,CollectDataDay)
+    
     # Holidays only add the data and do not change the infection numbers
     if Day in range (30,37):
         
@@ -42,82 +65,85 @@ for Day in range(0,len(DateRange)):
         R.append(SIRvalsToday[2])
                 
         # Collects whatever data you want at the start of the reading week 1
-        if Day == 31:
+        if Day == 31:            
             
             # Randomly Infects 10 percent of the pupolation when they travel home - this function can be tailored to wight certain ethnicities 
-            for StudentNumber in range(round(s.PopulationSize/10)):                 
+            for StudentNumber in range(0,round(s.PopulationSize/20)):                 
                     f.InfectRandomStudent(Day,DateRange)
                     
-            #Calling function SIRinfofilter to find out who was infected in the onrth halls at the start of the reading week 
-            (SIRlistNorth), (SIRlistEast), (SIRlistSouth) , (SIRlistWest) = m.DailyDataSIR()#m.SIRinfoFilter('I', None, None, 'North', None)
-        continue
+            #Calling function SIRinfofilter to find out who was infected in the onrth halls at the start of the reading week             
+            
+        else:
+            continue
             
         # Christomas hols
-    if Day in range (100,150):        
+    elif Day in range (90,120):        
         SIRvalsToday = m.CollectSIRdata()
         S.append(SIRvalsToday[0])
         I.append(SIRvalsToday[1])
         R.append(SIRvalsToday[2])        
-        if Day == 145:           
-            for StudentNumber in range(round(s.PopulationSize/5)):                   
+        if Day == 118:           
+            for StudentNumber in range(0,round(s.PopulationSize/90)):                   
                    f.InfectRandomStudent(Day,DateRange)                    
-        elif Day == 102:
-            
-            # Collects whatever data you want at the start of the Christmas holidays
-            ChristmasData = m.SIRinfoFilter('I', None, None, 'North', None)     
-        continue    
-    if Day in range (200,230):        
+        elif Day == 119:         
+           
+            # Collects whatever data you want at the start of the Christmas holidays- if you want data on a day other than a collect data day then call today as the collectdataday
+            ChristmasData = m.SIRinfoFilter('I', None, None, None, None, Day,Day)     
+        else:
+            continue    
+        
+    #   Easter hols
+    elif Day in range (200,214):        
         SIRvalsToday = m.CollectSIRdata()
         S.append(SIRvalsToday[0])
         I.append(SIRvalsToday[1])
         R.append(SIRvalsToday[2])
         
         # Collects whatever data you want at the start of the reading week 2
-        if Day == 225:            
-            for StudentNumber in range(round(s.PopulationSize/5)):                 
+        if Day == 213:            
+            for StudentNumber in range(0,round(s.PopulationSize/17)):                 
                     f.InfectRandomStudent(Day,DateRange)                   
-            ReadingWeek2Data = m.SIRinfoFilter(None, None, None, None, None)
-        continue
-     
-    # Week days add normal infecton data            
-    if 0<= Day%7 <=5:
+            ReadingWeek2Data = m.SIRinfoFilter('I', None, None, None, None,Day,Day)
+        else:
+            continue
+    else:
+        # Week days add normal infecton data            
+        if 0<= Day%7 <=5:
+            
+            SIRvalsToday = m.CollectSIRdata()
+            S.append(SIRvalsToday[0])
+            I.append(SIRvalsToday[1])
+            R.append(SIRvalsToday[2])               
+            f.infected( Day , DateRange)           
+            continue
         
-        SIRvalsToday = m.CollectSIRdata()
-        S.append(SIRvalsToday[0])
-        I.append(SIRvalsToday[1])
-        R.append(SIRvalsToday[2])               
-        f.infected( Day , DateRange)           
-        continue
+        # Weekends add an extra 2 people to everyones social number
+        elif 6<= Day%7 <=7:       
+            SIRvalsToday = m.CollectSIRdata()
+            S.append(SIRvalsToday[0])
+            I.append(SIRvalsToday[1])
+            R.append(SIRvalsToday[2])       
+            for StudentNumber in range(s.PopulationSize):            
+                s.StudentObjects[StudentNumber].Social = (s.StudentObjects[StudentNumber].Social) +2                  
+            f.infected( Day , DateRange)         
+            for StudentNumber in range(s.PopulationSize):
+                s.StudentObjects[StudentNumber].Social = (s.StudentObjects[StudentNumber].Social) -2              
+            continue   
     
-    # Weekends add an extra 2 people to everyones social number
-    elif 6<= Day%7 <=7:       
-        SIRvalsToday = m.CollectSIRdata()
-        S.append(SIRvalsToday[0])
-        I.append(SIRvalsToday[1])
-        R.append(SIRvalsToday[2])       
-        for StudentNumber in range(s.PopulationSize):            
-            s.StudentObjects[StudentNumber].Social = (s.StudentObjects[StudentNumber].Social) +2                  
-        f.infected( Day , DateRange)         
-        for StudentNumber in range(s.PopulationSize):
-            s.StudentObjects[StudentNumber].Social = (s.StudentObjects[StudentNumber].Social) -2            
-        continue   
 SIRSeries['S'] = S
 SIRSeries['I'] = I           
 SIRSeries['R'] = R
+SIRSeries.set_index('Date',inplace=True)
+
 
 #  Look at certain periods of time
-SIRSeries = SIRSeries.drop(labels=range(130, 322 ), axis=0)
+# SIRSeries = SIRSeries.drop(labels=range(200, 365 ), axis=0)
+
+m.MainTimeSeriesPlot(SIRSeries)
 
 
 
-SIRSeries.set_index('Date',inplace=True)
-SIRSeries.plot()
 
-plt.figure()
-df = pd.DataFrame(vars(SIRlistNorth))
-print(df)
-df.plot.bar(x="Halls", y="I", rot=70, title="graph1");
-plt.plot()
 plt.show
 
 # SIRSeries.pivot(index='Date', columns='SIR', values='y')
